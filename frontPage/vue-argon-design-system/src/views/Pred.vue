a66a91d1a9bc3c81514e611d254dc79dd7e79706
McGeeForest <shulin.zou@qq.com>
2022-03-18 15:34:15 +0800

    删除部分多余div和输出

--- frontPage/vue-argon-design-system/src/views/Pred.vue
+++ frontPage/vue-argon-design-system/src/views/Pred.vue
@@ -10,33 +10,24 @@
                         <div class="row" >
                             <div class="col">
                                 <h1 class="display-3  text-white">输入问题执行预测</h1>
-                                <section class="">
-                                    <div class="">
-                                        <div class="">
-                                            <div class="">
-                                                <card gradient="secondary" shadow body-classes="">
-                                                    <base-input 
-                                                                v-model="questionBody.title"
-                                                                class=""
-                                                                alternative
-                                                                placeholder="问题标题"
-                                                                addon-left-icon="ni ni-user-run">
-                                                    </base-input>
-                                                    <base-input class="">
-                                                            <textarea  
-                                                                v-model="questionBody.content" 
-                                                                class="form-control form-control-alternative" 
-                                                                name="name" 
-                                                                rows="4"
-                                                                cols="80" placeholder="问题内容"></textarea>
-                                                    </base-input>
-                                                    <base-button v-on:click="uploadQuestion" type="default" round block size="lg" >
-                                                        提交问题
-                                                    </base-button>
-                                                </card>
-                                            </div>
-                                        </div>
-                                    </div>
+                                <section>
+                                    <card gradient="secondary" shadow body-classes="">
+                                        <base-input 
+                                            v-model="questionBody.title"
+                                            alternative
+                                            placeholder="问题标题"
+                                            addon-left-icon="ni ni-user-run">
+                                        </base-input>
+                                        <base-input >
+                                            <textarea  
+                                                v-model="questionBody.content" 
+                                                class="form-control form-control-alternative" 
+                                                name="name" 
+                                                rows="4"
+                                                cols="80" placeholder="问题内容"></textarea>
+                                        </base-input>
+                                        <base-button v-on:click="uploadQuestion" type="default" round block size="lg" > 提交问题 </base-button>
+                                    </card>
                                 </section>
                             </div>
                             <div class="col hidden-y" style="margin-top: -49px;height:400px;"  >
@@ -65,14 +56,13 @@
                     <div class="row">
                         <!-- {{item}} -->
                         <div class="col-2" style="float:left;margin-top:5px">
-                            <small style="margin-left:5px">{{formatDate(item[1]*1000)[1]}}</small>
-                            <badge type="success">{{formatDate(item[1]*1000)[0]}}</badge>
+                            <small style="margin-left:5px">{{formatDate_use(item[1]*1000)[1]}}</small>
+                            <badge type="success">{{formatDate_use(item[1]*1000)[0]}}</badge>
                         </div>
                         <div style="height:95px" class="hidden-y col-10">
                             <small>{{item[0]}}</small>
                         </div>
                     </div>
-
                     <hr style="margin-top:3px;margin-bottom:3px">
                 </div>
             </div>
@@ -102,22 +92,25 @@ export default {
   },
   methods: {
       uploadQuestion: function(){
-          console.log(this.questionBody)
+        //   console.log(this.questionBody)
           this.$axios.post("/predict", this.questionBody).then(res=>{
-              console.log(res.data)
+            //   console.log(res.data)
               this.expertList = res.data
           })
       },
       showHistory: function(item){
-          console.log("查看用户"+item[0])
+        //   console.log("查看用户"+item[0])
           this.$axios.get("/userHistoryById", {params: {
               userId: item[0]
           }}).then(res=>{
-              console.log(res)
+            //   console.log(res)
               this.historyList = res.data
               this.viewHistory=true
-              console.log(this.viewHistory)
+            //   console.log(this.viewHistory)
           })
+      },
+      formatDate_use: function(item){
+          return this.$FormatDate(item)
       }
 
   }
